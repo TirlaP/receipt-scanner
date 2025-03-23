8c7349f486413b3a36d9e37f7c0c67bfc20911ca
TirlaP <46626173+TirlaP@users.noreply.github.com>
2025-03-23 11:28:47 +0000

    Deploying to gh-pages from @ TirlaP/receipt-scanner@28941f315815db5daf23d796ffdfee2d2e9d38a0 🚀

---
+++ config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCNRUWxYDEaNG0YrFfzfZSmMff2A4XjtcY",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
